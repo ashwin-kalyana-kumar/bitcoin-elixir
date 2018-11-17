1124dad4767bfe748ff0ea57e327ef63d024be91
Ashwin Kalyana Kumar <as.kalyanakumar@ufl.edu>
2018-11-17 18:15:43 -0500

    added functions to build a transaction

--- lib/bitcoin.ex
+++ lib/bitcoin.ex
@@ -2,17 +2,4 @@ defmodule Bitcoin do
   @moduledoc """
   Documentation for Bitcoin.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Bitcoin.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/bitcoin_test.exs
+++ test/bitcoin_test.exs
@@ -2,7 +2,12 @@ defmodule BitcoinTest do
   use ExUnit.Case
   doctest Bitcoin
 
-  test "greets the world" do
-    assert Bitcoin.hello() == :world
+  test "digitally sign and verify" do
+    {public, private} = Crypto.CryptoModule.get_key_pair()
+    assert is_binary(public) == true
+    assert is_binary(private) == true
+    message = "hello world"
+    signature = Crypto.CryptoModule.digital_sign(private, message)
+    assert Crypto.CryptoModule.verify_sign(public, message, signature) == true
   end
 end
