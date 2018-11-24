b78462c6b11cd59e17a70c24c4f054c640bdc27b
Ashwin Kalyana Kumar <as.kalyanakumar@ufl.edu>
2018-11-24 18:46:07 -0500

    removed conflict

--- lib/Mint Processor/mint_structure.ex
+++ lib/Mint Processor/mint_structure.ex
@@ -2,10 +2,6 @@ defmodule MintProcessor.Structure do
   defstruct [
     :unverified_transaction,
     :unused_transaction,
-<<<<<<< HEAD
-    :used_transaction,
-=======
->>>>>>> origin/mint-next-changes-jin
     :mint_blockchain,
     :mint_tx_map
   ]
