e315b1e5bfcfe9e4b0aed2abb2929036a26c33b2
jinansh94 <33523477+jinansh94@users.noreply.github.com>
2018-11-24 16:56:45 -0500

    commit_4
    chnages to user supervisor

--- lib/User/bitcoin_supervisor.ex
+++ lib/User/bitcoin_supervisor.ex
@@ -1,4 +1,4 @@
-defmodule User.Supervisor do
+defmodule User.BitcoinSupervisor do
 
   use DynamicSupervisor
 
@@ -6,34 +6,49 @@ defmodule User.Supervisor do
     DynamicSupervisor.start_link(strategy: :one_for_one, name: :user_super)
   end
 
-  def start_child(n,m_pid,total_node) when n == 1 do
+  def update_intial_neighbours(node_map) do
+    list = DynamicSupervisor.which_children(:user_super)
+
+    list
+    |> Enum.each(fn item ->
+      {_,pid,_,_} = item
+      GenServer.cast(pid,{:update_neighbors,node_map})
+      end)
+  end
+
+  def start_child(n,m_pid,total_node,node_map) when n == 1 do
+    blockchain = GenServer.call(m_pid,{:get_blockchain})
     neighbour = %User.NeighborStruct{left_guy: total_node,right_guy: n+1,random_guy: :rand.uniform(total_node)}
-    spec = %{id: 1, restart: :temporary, start: {User.BitcoinUser, :start_link, [n,m_pid,neighbour,#how to add blockchain]}}
-    {:ok, _child} = DynamicSupervisor.start_child(:user_super, spec)
+    spec = %{id: 1, restart: :temporary, start: {User.BitcoinUser, :start_link, [n,m_pid,neighbour,blockchain]}}
+    {:ok, child} = DynamicSupervisor.start_child(:user_super, spec)
+    update_node_map = Map.put_new(node_map, n, child)
+    spawn(fn -> update_intial_neighbours(update_node_map) end)
   end
 
-  def start_child(n,m_pid,total_node) do
+  def start_child(n,m_pid,total_node,node_map) do
+    blockchain = GenServer.call(m_pid,{:get_blockchain})
+    neighbour =
     if n == 100 do
-      neighbour = %User.NeighborStruct{left_guy: n-1,right_guy: 1,random_guy: :rand.uniform(total_node)}
+      %User.NeighborStruct{left_guy: n-1,right_guy: 1,random_guy: :rand.uniform(total_node)}
     else
-      neighbour = %User.NeighborStruct{left_guy: n-1,right_guy: n+1,random_guy: :rand.uniform(total_node)}
+      %User.NeighborStruct{left_guy: n-1,right_guy: n+1,random_guy: :rand.uniform(total_node)}
     end
-    spec = %{id: n, restart: :temporary, start: {User.BitcoinUser, :start_link, [n,m_pid,neighbour,#how to add blockchain]}}
-    {:ok, _child} = DynamicSupervisor.start_child(:user_super, spec)
-    start_child(n-1,m_pid,total_node)
+    spec = %{id: n, restart: :temporary, start: {User.BitcoinUser, :start_link, [n,m_pid,neighbour,blockchain]}}
+    {:ok, child} = DynamicSupervisor.start_child(:user_super, spec)
+    update_node_map = Map.put_new(node_map, n, child)
+    start_child(n-1,m_pid,total_node,update_node_map)
   end
 
-  def add_new_node() do
-    total_children = count_children(user_super)
-    node_to_add = :random.uniform(total_children)
-    cond do
-      node_to_add == 1 -> # add
-      node_to_add == 100 -> #add
-      tru -> #add
-    else
-
-    end
+  def add_new_node(m_pid) do
+    total_children = DynamicSupervisor.count_children(:user_super)
+    {_,node_to_add,_,_} = Enum.random(total_children)
+    {_,random_neighbour,_,_} = Enum.random(total_children)
+    node_to_add_neighbours = Genserver.call(node_to_add,{:get_neighbours})
+    neighbour =  %User.NeighborStruct{left_guy: node_to_add, right_guy: node_to_add_neighbours.right_guy, random_guy: random_neighbour}
+    blockchain = GenServer.call(m_pid,{:get_blockchain})
+    spec = %{id: total_children+1, restart: :temporary, start: {User.BitcoinUser, :start_link, [total_children+1,m_pid,neighbour,blockchain]}}
+    {:ok, child} = DynamicSupervisor.start_child(:user_super, spec)
+    GenServer.cast(node_to_add,{:update_neighbours_dueto_new_node,{:right_negh,child}})
+    GenServer.cast(node_to_add_neighbours.right_guy,{:update_neighbours_dueto_new_node,{:left_negh,child}})
   end
-
-
 end
