54f8dc9d604531fee36e733a6f826ef63bae94f8
jinansh94 <33523477+jinansh94@users.noreply.github.com>
2018-11-24 18:34:53 -0500

    Added very_tx block in mint_genserver

--- lib/Mint Processor/mint_genserver.ex
+++ lib/Mint Processor/mint_genserver.ex
@@ -6,9 +6,9 @@ defmodule MintProcessor.MintGenServer do
 
     uv_tx = %{}
     us_tx = %{}
-    used_tx = []
+    tx_map = %{}
 
-    mint_state = %MintProcessor.Structure{unverified_transaction: uv_tx, unused_transaction: us_tx, used_transaction: used_tx}
+    mint_state = %MintProcessor.Structure{unverified_transaction: uv_tx, unused_transaction: us_tx, mint_tx_map: tx_map}
     GenServer.start_link(__MODULE__,mint_state)
   end
 
@@ -28,18 +28,94 @@ defmodule MintProcessor.MintGenServer do
     update_uvtx_used_tx(head, tail, update_uv_tx)
   end
 
-  def handle_cast({:tx_happened, tx_id},mint_state) do
-    # the whole transaction
+  def verify_integrity_of_tx(curr_tx_check, other_tx_list, curr_amount, total_amount, tx_map, unused_tx_list) when other_tx_list == [] do
+    flag = Map.get(unused_tx_list,curr_tx_check.txid)
+
+    if flag == -1 do
+      transaction = Map.get(tx_map, curr_tx_check.txid, nil)
+      if transaction == nil do
+        false
+      else
+        cond do
+          transaction.transaction_output.pub_key_script == curr_tx_check.public_key_hash ->
+            curr_amount = curr_amount + transaction.transaction_output.amount
+            if curr_amount >= total_amount do
+              true
+            else
+              false
+            end
+
+          transaction.transaction_output.sender_pub_key_script == curr_tx_check.public_key_hash ->
+            curr_amount = curr_amount + transaction.transaction_output.got_back_amount
+            if curr_amount >= total_amount do
+              true
+            else
+              false
+            end
+
+          true ->
+            false
+        end
+      end
+    else
+      false
+    end
+
+  end
+
+  def verify_integrity_of_tx(curr_tx_check, other_tx_list, curr_amount, total_amount, tx_map, unused_tx_list) do
+    flag = Map.get(unused_tx_list,curr_tx_check.txid)
+
+    if flag == -1 do
+      transaction = Map.get(tx_map, curr_tx_check.txid, nil)
+      if transaction == nil do
+        false
+      else
+        cond do
+          transaction.transaction_output.pub_key_script == curr_tx_check.public_key_hash ->
+            [head | tail] = other_tx_list
+            curr_amount = curr_amount + transaction.transaction_output.amount
+            verify_integrity_of_tx(head, tail, curr_amount, total_amount, tx_map, unused_tx_list)
+
+          transaction.transaction_output.sender_pub_key_script == curr_tx_check.public_key_hash ->
+            [head | tail] = other_tx_list
+            curr_amount = curr_amount + transaction.transaction_output.got_back_amount
+            verify_integrity_of_tx(head, tail, curr_amount, total_amount, tx_map, unused_tx_list)
+
+          true ->
+            false
+        end
+      end
+    else
+      false
+    end
+
+  end
+
+  def handle_cast({:tx_happened, transaction},mint_state) do
     old_uv_tx = mint_state.unverified_transaction
+    old_tx_map = mint_state.mint_tx_map
 
-    # Verify Signature
+    #verify Transaction before adding
+    sign = transaction.signature
+    transaction = transaction |> Map.put(:signature, nil)
 
-    new_uv_tx = Map.put_new(old_uv_tx, tx_id, -1)
+    authentic =
+      Crypto.CryptoModule.verify_transaction_sign(transaction.public_key, transaction, sign)
 
-    mint_state = mint_state |>
-                  Map.update!(:unverified_transaction, fn _x -> new_uv_tx end)
+    new_mint_state =
+    if(authentic) do
+      new_uv_tx = Map.put(old_uv_tx, transaction.txid, -1)
+      new_tx_map = Map.put(old_tx_map, transaction.txid, transaction)
 
-    {:noreply,mint_state}
+      mint_state
+      |> Map.update!(:unverified_transaction, fn _x -> new_uv_tx end)
+      |> Map.update!(:mint_tx_map, fn _x -> new_tx_map end)
+    else
+      mint_state
+    end
+
+    {:noreply,new_mint_state}
   end
 
   def handle_cast({:block_generated, tx_used_list, _block}, mint_state) do
@@ -61,9 +137,15 @@ defmodule MintProcessor.MintGenServer do
     {:noreply,new_mint_state}
   end
 
-  def handle_call({:verify_unspent_tx,unspent_tx_list, amount, pb_key_script},_from,mint_state) do
+  def handle_call({:verify_unspent_tx,tx_input_list, amount},_from,mint_state) do
+    [head | tail] = tx_input_list
+    curr_amount = 0
+    tx_map = mint_state.mint_tx_map
+    unused_tx_list = mint_state.unused_transaction
 
-    # tru false return
+    flag = verify_integrity_of_tx(head, tail, curr_amount, amount, tx_map, unused_tx_list)
+
+    {:reply,flag,mint_state}
   end
 
   def handle_call({:get_blockchain},_from,mint_state) do
--- lib/Mint Processor/mint_structure.ex
+++ lib/Mint Processor/mint_structure.ex
@@ -2,7 +2,7 @@ defmodule MintProcessor.Structure do
   defstruct [
     :unverified_transaction,
     :unused_transaction,
-    :used_transaction,
-    :mint_blockchain
+    :mint_blockchain,
+    :mint_tx_map
   ]
 end
