d6799f98f89aff8d6e03f3eb1742bdaca340f825
Ashwin Kalyana Kumar <as.kalyanakumar@ufl.edu>
2018-11-26 21:34:36 -0500

    changes for test cases

--- test/bitcoin_test.exs
+++ test/bitcoin_test.exs
@@ -2,39 +2,43 @@ defmodule BitcoinTest do
   use ExUnit.Case
   doctest Bitcoin
 
-  """
-    test "digitally sign and verify" do
-      {public, private} = Crypto.CryptoModule.get_key_pair()
-      assert is_binary(public) == true
-      assert is_binary(private) == true
-      message = %{a1: "a", a2: "b"}
-      signature = Crypto.CryptoModule.digital_sign(private, message)
-      assert Crypto.CryptoModule.verify_sign(public, message, signature) == true
-    end
+  test "public and private key generation" do
+    {public, private} = Crypto.CryptoModule.get_key_pair()
+    assert is_binary(public) == true
+    assert is_binary(private) == true
+  end
 
-    test "create and verify transaction" do
-      {public, private} = Crypto.CryptoModule.get_key_pair()
-      {public1, private1} = Crypto.CryptoModule.get_key_pair()
-      pubkey_hash = Crypto.CryptoModule.hash_key(public)
-      pubkey_hash1 = Crypto.CryptoModule.hash_key(public1)
+  test "digitally sign and verify" do
+    {public, private} = Crypto.CryptoModule.get_key_pair()
+    assert is_binary(public) == true
+    assert is_binary(private) == true
+    message = "hello world!"
+    signature = Crypto.CryptoModule.digital_sign(private, message)
+    assert Crypto.CryptoModule.verify_sign(public, message, signature) == true
+  end
 
-      input_txn1 =
-        User.BlockGenerator.generate_coinbase_transaction(50, private, public, pubkey_hash)
+  test "create and verify transaction" do
+    {public, private} = Crypto.CryptoModule.get_key_pair()
+    {public1, private1} = Crypto.CryptoModule.get_key_pair()
+    pubkey_hash = Crypto.CryptoModule.hash_key(public)
+    pubkey_hash1 = Crypto.CryptoModule.hash_key(public1)
 
-      transaction =
-        User.BitcoinUser.build_transaction(
-          [input_txn1],
-          20,
-          pubkey_hash1,
-          30,
-          pubkey_hash,
-          public,
-          private
-        )
+    input_txn1 =
+      User.BlockGenerator.generate_coinbase_transaction(50, private, public, pubkey_hash)
 
-      assert User.BitcoinUser.check_authenticity_of_txn(transaction) == :authentic
-    end
-  """
+    transaction =
+      User.BitcoinUser.build_transaction(
+        [input_txn1],
+        20,
+        pubkey_hash1,
+        30,
+        pubkey_hash,
+        public,
+        private
+      )
+
+    assert User.BitcoinUser.check_authenticity_of_txn(transaction) == :authentic
+  end
 
   test "create and verify block" do
     {public, private} = Crypto.CryptoModule.get_key_pair()
@@ -57,6 +61,6 @@ defmodule BitcoinTest do
         nil
       )
 
-    User.BitcoinUser.verify_block([], block)
+    assert(User.BitcoinUser.verify_block([], block) == :valid)
   end
 end
